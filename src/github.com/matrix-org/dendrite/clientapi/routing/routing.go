7c601c1a26fe9631142d0cbab8ec03ba01e47d45
Kegan Dougal <kegan@matrix.org>
2017-02-17 17:34:17 +0000

    Use const

--- src/github.com/matrix-org/dendrite/clientapi/routing/routing.go
+++ src/github.com/matrix-org/dendrite/clientapi/routing/routing.go
@@ -20,7 +20,7 @@ func make(metricsName string, h util.JSONRequestHandler) http.Handler {
 // to clients which need to make outbound HTTP requests.
 func Setup(servMux *http.ServeMux, httpClient *http.Client) {
 	apiMux := mux.NewRouter()
-	r0mux := apiMux.PathPrefix("/_matrix/client/r0").Subrouter()
+	r0mux := apiMux.PathPrefix(pathPrefixR0).Subrouter()
 	r0mux.Handle("/sync", make("sync", &readers.Sync{}))
 	r0mux.Handle("/rooms/{roomID}/send/{eventType}", make("send_message", &writers.SendMessage{}))
 
