a5c562efd3dc5f71f3ef4eb9d8e6d69d8dc72a9b
Kegan Dougal <kegan@matrix.org>
2017-02-20 16:44:58 +0000

    Add error package and some Matrix errors
    Terse function names are used to make usage not stutter. For example:
    
    ```go
    err := error.Forbidden("you shall not pass")
    ```
    
    At the moment they are all the same fundamental `MatrixError` type. This will
    be bad if we ever want to `switch` based on the kind of error. I'm hoping we
    won't ever need to introspect into errors like this: ideally these errors would
    be created purely for immediately being returned in an HTTP response.
    
    `MatrixError` implements the `error` interface.

---
+++ src/github.com/matrix-org/dendrite/clientapi/error/error_test.go
@@ -0,0 +1,30 @@
+package error
+
+import (
+	"encoding/json"
+	"testing"
+)
+
+func TestLimitExceeded(t *testing.T) {
+	e := LimitExceeded("too fast", 5000)
+	jsonBytes, err := json.Marshal(&e)
+	if err != nil {
+		t.Fatalf("TestLimitExceeded: Failed to marshal LimitExceeded error. %s", err.Error())
+	}
+	want := `{"errcode":"M_LIMIT_EXCEEDED","error":"too fast","retry_after_ms":5000}`
+	if string(jsonBytes) != want {
+		t.Errorf("TestLimitExceeded: want %s, got %s", want, string(jsonBytes))
+	}
+}
+
+func TestForbidden(t *testing.T) {
+	e := Forbidden("you shall not pass")
+	jsonBytes, err := json.Marshal(&e)
+	if err != nil {
+		t.Fatalf("TestForbidden: Failed to marshal Forbidden error. %s", err.Error())
+	}
+	want := `{"errcode":"M_FORBIDDEN","error":"you shall not pass"}`
+	if string(jsonBytes) != want {
+		t.Errorf("TestForbidden: want %s, got %s", want, string(jsonBytes))
+	}
+}
