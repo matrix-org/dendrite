17cc782affbcac3a2b72733b1c9b572f7dde1d4c
Kegan Dougal <kegan@matrix.org>
2017-02-22 11:01:21 +0000

    'verify' the access token

--- src/github.com/matrix-org/dendrite/clientapi/readers/sync.go
+++ src/github.com/matrix-org/dendrite/clientapi/readers/sync.go
@@ -3,13 +3,22 @@ package readers
 import (
 	"net/http"
 
+	"github.com/matrix-org/dendrite/clientapi/auth"
 	"github.com/matrix-org/util"
 )
 
 // Sync implements /sync
 func Sync(req *http.Request) (interface{}, *util.HTTPError) {
 	logger := util.GetLogger(req.Context())
-	logger.Info("Doing stuff...")
+	userID, err := auth.VerifyAccessToken(req)
+	if err != nil {
+		return nil, &util.HTTPError{
+			Code: 403,
+			JSON: err,
+		}
+	}
+
+	logger.WithField("userID", userID).Info("Doing stuff...")
 	return nil, &util.HTTPError{
 		Code:    404,
 		Message: "Not implemented yet",
--- src/github.com/matrix-org/dendrite/clientapi/writers/sendmessage.go
+++ src/github.com/matrix-org/dendrite/clientapi/writers/sendmessage.go
@@ -3,13 +3,26 @@ package writers
 import (
 	"net/http"
 
+	log "github.com/Sirupsen/logrus"
+	"github.com/matrix-org/dendrite/clientapi/auth"
 	"github.com/matrix-org/util"
 )
 
 // SendMessage implements /rooms/{roomID}/send/{eventType}
 func SendMessage(req *http.Request, roomID, eventType string) (interface{}, *util.HTTPError) {
 	logger := util.GetLogger(req.Context())
-	logger.WithField("roomID", roomID).WithField("eventType", eventType).Info("Doing stuff...")
+	userID, err := auth.VerifyAccessToken(req)
+	if err != nil {
+		return nil, &util.HTTPError{
+			Code: 403,
+			JSON: err,
+		}
+	}
+	logger.WithFields(log.Fields{
+		"roomID":    roomID,
+		"eventType": eventType,
+		"userID":    userID,
+	}).Info("Doing stuff...")
 	return nil, &util.HTTPError{
 		Code:    404,
 		Message: "Not implemented yet",
