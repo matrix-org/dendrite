cce5af46bade030f0138b59f7b6129247ca2f065
Mark Haines <mjark@negativecurvature.net>
2017-03-06 14:26:32 +0000

    Warn people against requesting empty state keys by accident

--- src/github.com/matrix-org/dendrite/roomserver/api/query.go
+++ src/github.com/matrix-org/dendrite/roomserver/api/query.go
@@ -9,8 +9,14 @@ import (
 )
 
 // StateKeyTuple is a pair of an event type and state_key.
+// This is used when requesting parts of the state of a room.
 type StateKeyTuple struct {
-	EventType     string
+	// The "type" key
+	EventType string
+	// The "state_key" of a matrix event.
+	// The empty string is a legitimate value for the "state_key" in matrix
+	// so take care to initialise this field lest you accidentally request a
+	// "state_key" with the go default of the empty string.
 	EventStateKey string
 }
 
