84682b33c968f469d201bbe08b90b4ff6e120dd4
Mark Haines <mjark@negativecurvature.net>
2017-03-08 13:27:21 +0000

    Use Unique from github.com/matrix-org/util (#28)
    * Update github.com/matrix-org/util
    
    * Use Unique from github.com/matrix-org/util

--- src/github.com/matrix-org/dendrite/roomserver/input/state_test.go
+++ src/github.com/matrix-org/dendrite/roomserver/input/state_test.go
@@ -5,32 +5,6 @@ import (
 	"testing"
 )
 
-type sortBytes []byte
-
-func (s sortBytes) Len() int           { return len(s) }
-func (s sortBytes) Less(i, j int) bool { return s[i] < s[j] }
-func (s sortBytes) Swap(i, j int)      { s[i], s[j] = s[j], s[i] }
-
-func TestUnique(t *testing.T) {
-	testCases := []struct {
-		Input string
-		Want  string
-	}{
-		{"", ""},
-		{"abc", "abc"},
-		{"aaabbbccc", "abc"},
-	}
-
-	for _, test := range testCases {
-		input := []byte(test.Input)
-		want := string(test.Want)
-		got := string(input[:unique(sortBytes(input))])
-		if got != want {
-			t.Fatal("Wanted ", want, " got ", got)
-		}
-	}
-}
-
 func TestFindDuplicateStateKeys(t *testing.T) {
 	testCases := []struct {
 		Input []types.StateEntry
