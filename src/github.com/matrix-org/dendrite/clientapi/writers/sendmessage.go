f54fda1d3f4aa01f014460c67734459c7dfecaa8
Kegan Dougal <kegan@matrix.org>
2017-03-14 14:11:54 +0000

    Query the roomserver for the current room state
    We need this to send messages.

--- src/github.com/matrix-org/dendrite/clientapi/writers/sendmessage.go
+++ src/github.com/matrix-org/dendrite/clientapi/writers/sendmessage.go
@@ -3,22 +3,54 @@ package writers
 import (
 	"net/http"
 
+	"fmt"
 	log "github.com/Sirupsen/logrus"
 	"github.com/matrix-org/dendrite/clientapi/auth"
+	"github.com/matrix-org/dendrite/clientapi/config"
+	"github.com/matrix-org/dendrite/clientapi/httputil"
+	"github.com/matrix-org/dendrite/common"
+	"github.com/matrix-org/dendrite/roomserver/api"
 	"github.com/matrix-org/util"
 )
 
-// SendMessage implements /rooms/{roomID}/send/{eventType}
-func SendMessage(req *http.Request, roomID, eventType string) util.JSONResponse {
+// http://matrix.org/docs/spec/client_server/r0.2.0.html#put-matrix-client-r0-rooms-roomid-send-eventtype-txnid
+type sendMessageResponse struct {
+	EventID string `json:"event_id"`
+}
+
+// SendMessage implements /rooms/{roomID}/send/{eventType}/{txnID}
+func SendMessage(req *http.Request, roomID, eventType, txnID string, cfg config.ClientAPI, queryAPI api.RoomserverQueryAPI) util.JSONResponse {
 	logger := util.GetLogger(req.Context())
 	userID, resErr := auth.VerifyAccessToken(req)
 	if resErr != nil {
 		return *resErr
 	}
+	var r map[string]interface{} // must be a JSON object
+	resErr = httputil.UnmarshalJSONRequest(req, &r)
+	if resErr != nil {
+		return *resErr
+	}
+	eventID := fmt.Sprintf("$%s:%s", util.RandomString(16), cfg.ServerName)
+
+	queryReq := api.QueryLatestEventsAndStateRequest{
+		RoomID: roomID,
+		StateToFetch: []common.StateKeyTuple{
+			{"m.room.member", userID},
+		},
+	}
+	var queryRes api.QueryLatestEventsAndStateResponse
+	if err := queryAPI.QueryLatestEventsAndState(&queryReq, &queryRes); err != nil {
+		return httputil.LogThenError(req, err)
+	}
+
 	logger.WithFields(log.Fields{
 		"roomID":    roomID,
 		"eventType": eventType,
 		"userID":    userID,
+		"res":       queryRes,
 	}).Info("Doing stuff...")
-	return util.MessageResponse(404, "Not implemented yet")
+	return util.JSONResponse{
+		Code: 200,
+		JSON: sendMessageResponse{eventID},
+	}
 }
