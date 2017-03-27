edd7feecf0c6dded9e9b80abd1ae6f392c2f69b2
Kegan Dougal <kegan@matrix.org>
2017-03-27 14:42:20 +0100

    Write OutputRoomEvents into the database and remember the arrival order
    The arrival order will eventually be used to generate streaming tokens to
    return to the client as a sync token.

--- src/github.com/matrix-org/dendrite/clientapi/sync/syncserver.go
+++ src/github.com/matrix-org/dendrite/clientapi/sync/syncserver.go
@@ -1,19 +1,25 @@
 package sync
 
 import (
+	"encoding/json"
+
 	log "github.com/Sirupsen/logrus"
 	"github.com/matrix-org/dendrite/clientapi/config"
+	"github.com/matrix-org/dendrite/clientapi/storage"
 	"github.com/matrix-org/dendrite/common"
+	"github.com/matrix-org/dendrite/roomserver/api"
+	"github.com/matrix-org/gomatrixserverlib"
 	sarama "gopkg.in/Shopify/sarama.v1"
 )
 
 // Server contains all the logic for running a sync server
 type Server struct {
 	roomServerConsumer *common.ContinualConsumer
+	db                 *storage.SyncServerDatabase
 }
 
 // NewServer creates a new sync server. Call Start() to begin consuming from room servers.
-func NewServer(cfg *config.Sync, store common.PartitionStorer) (*Server, error) {
+func NewServer(cfg *config.Sync, store *storage.SyncServerDatabase) (*Server, error) {
 	kafkaConsumer, err := sarama.NewConsumer(cfg.KafkaConsumerURIs, nil)
 	if err != nil {
 		return nil, err
@@ -26,6 +32,7 @@ func NewServer(cfg *config.Sync, store common.PartitionStorer) (*Server, error) 
 	}
 	s := &Server{
 		roomServerConsumer: &consumer,
+		db:                 store,
 	}
 	consumer.ProcessMessage = s.onMessage
 
@@ -38,6 +45,28 @@ func (s *Server) Start() error {
 }
 
 func (s *Server) onMessage(msg *sarama.ConsumerMessage) error {
-	log.WithField("key", string(msg.Key)).WithField("val", string(msg.Value)).Info("Recv")
+	// Parse out the event JSON
+	var output api.OutputRoomEvent
+	if err := json.Unmarshal(msg.Value, &output); err != nil {
+		// If the message was invalid, log it and move on to the next message in the stream
+		log.WithError(err).Errorf("roomserver output log: message parse failure")
+		return nil
+	}
+
+	ev, err := gomatrixserverlib.NewEventFromTrustedJSON(output.Event, false)
+	if err != nil {
+		log.WithError(err).Errorf("roomserver output log: event parse failure")
+		return nil
+	}
+	log.WithFields(log.Fields{
+		"event_id": ev.EventID(),
+		"room_id":  ev.RoomID(),
+	}).Info("received event from roomserver")
+
+	if err := s.db.WriteEvent(&ev, output.AddsStateEventIDs, output.RemovesStateEventIDs); err != nil {
+		log.WithError(err).Errorf("roomserver output log: write event failure")
+		return nil
+	}
+
 	return nil
 }
