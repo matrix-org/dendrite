697252d87407b05495f6754fd82fe70920a2bbb5
Kegan Dougal <kegan@matrix.org>
2017-05-04 17:20:43 +0100

    Add syncserver integ test cmd and some WIP impl

--- travis-test.sh
+++ travis-test.sh
@@ -2,12 +2,14 @@
 
 set -eu
 
-# Check that the servers build
+# Check that the servers build (this is done explicitly because `gb build` can silently fail (exit 0) and then we'd test a stale binary)
 gb build github.com/matrix-org/dendrite/cmd/dendrite-room-server
 gb build github.com/matrix-org/dendrite/cmd/roomserver-integration-tests
+gb build github.com/matrix-org/dendrite/cmd/syncserver-integration-tests
 
 # Run the pre commit hooks
 ./hooks/pre-commit
 
 # Run the integration tests
 bin/roomserver-integration-tests
+bin/syncserver-integration-tests
