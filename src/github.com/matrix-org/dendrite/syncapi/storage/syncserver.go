7677d031a904ce9bec4297c337515047fc65ab5f
Kegan Dougal <kegan@matrix.org>
2017-05-11 15:02:28 +0100

    Add more syncserver tests
    Fixed exactly the same bug as before, but this time for incremental
    sync. Yay tests!

--- src/github.com/matrix-org/dendrite/syncapi/storage/syncserver.go
+++ src/github.com/matrix-org/dendrite/syncapi/storage/syncserver.go
@@ -129,6 +129,7 @@ func (d *SyncServerDatabase) IncrementalSync(userID string, fromPos, toPos types
 			if err != nil {
 				return err
 			}
+			state[roomID] = removeDuplicates(state[roomID], recentEvents)
 			roomData := types.RoomData{
 				State:        state[roomID],
 				RecentEvents: recentEvents,
@@ -170,28 +171,7 @@ func (d *SyncServerDatabase) CompleteSync(userID string, numRecentEventsPerRoom 
 				return err
 			}
 
-			// There may be some overlap where events in stateEvents are already in recentEvents, so filter
-			// them out so we don't include them twice in the /sync response. They should be in recentEvents
-			// only, so clients get to the correct state once they have rolled forward.
-			for _, recentEv := range recentEvents {
-				if recentEv.StateKey() == nil {
-					continue // not a state event
-				}
-				// TODO: This is a linear scan over all the current state events in this room. This will
-				//       be slow for big rooms. We should instead sort the state events by event ID  (ORDER BY)
-				//       then do a binary search to find matching events, similar to what roomserver does.
-				for j := 0; j < len(stateEvents); j++ {
-					if stateEvents[j].EventID() == recentEv.EventID() {
-						// overwrite the element to remove with the last element then pop the last element.
-						// This is orders of magnitude faster than re-slicing, but doesn't preserve ordering
-						// (we don't care about the order of stateEvents)
-						stateEvents[j] = stateEvents[len(stateEvents)-1]
-						stateEvents = stateEvents[:len(stateEvents)-1]
-						break // there shouldn't be multiple events with the same event ID
-					}
-				}
-
-			}
+			stateEvents = removeDuplicates(stateEvents, recentEvents)
 
 			data[roomID] = types.RoomData{
 				State:        stateEvents,
@@ -201,6 +181,31 @@ func (d *SyncServerDatabase) CompleteSync(userID string, numRecentEventsPerRoom 
 		return nil
 	})
 	return
+}
+
+// There may be some overlap where events in stateEvents are already in recentEvents, so filter
+// them out so we don't include them twice in the /sync response. They should be in recentEvents
+// only, so clients get to the correct state once they have rolled forward.
+func removeDuplicates(stateEvents, recentEvents []gomatrixserverlib.Event) []gomatrixserverlib.Event {
+	for _, recentEv := range recentEvents {
+		if recentEv.StateKey() == nil {
+			continue // not a state event
+		}
+		// TODO: This is a linear scan over all the current state events in this room. This will
+		//       be slow for big rooms. We should instead sort the state events by event ID  (ORDER BY)
+		//       then do a binary search to find matching events, similar to what roomserver does.
+		for j := 0; j < len(stateEvents); j++ {
+			if stateEvents[j].EventID() == recentEv.EventID() {
+				// overwrite the element to remove with the last element then pop the last element.
+				// This is orders of magnitude faster than re-slicing, but doesn't preserve ordering
+				// (we don't care about the order of stateEvents)
+				stateEvents[j] = stateEvents[len(stateEvents)-1]
+				stateEvents = stateEvents[:len(stateEvents)-1]
+				break // there shouldn't be multiple events with the same event ID
+			}
+		}
+	}
+	return stateEvents
 }
 
 func runTransaction(db *sql.DB, fn func(txn *sql.Tx) error) (err error) {
