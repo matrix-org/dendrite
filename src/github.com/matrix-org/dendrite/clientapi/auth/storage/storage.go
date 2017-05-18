9b48a67e141ec825fcf6bde1bd9ec06e97fd4842
Kegan Dougal <kegan@matrix.org>
2017-05-18 15:48:58 +0100

    Add AccountDatabase for storing user accounts
    Including the ability to add new accounts with a user/password and
    select accounts using a user/password. Uses bcrypt to hash passwords.

---
+++ src/github.com/matrix-org/dendrite/clientapi/auth/storage/storage.go
@@ -0,0 +1,67 @@
+// Copyright 2017 Vector Creations Ltd
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package storage
+
+import (
+	"database/sql"
+	"github.com/matrix-org/dendrite/clientapi/auth/types"
+	"github.com/matrix-org/gomatrixserverlib"
+	"golang.org/x/crypto/bcrypt"
+)
+
+// AccountDatabase represents an account database
+type AccountDatabase struct {
+	db       *sql.DB
+	accounts accountsStatements
+}
+
+// NewAccountDatabase creates a new accounts database
+func NewAccountDatabase(dataSourceName string, serverName gomatrixserverlib.ServerName) (*AccountDatabase, error) {
+	var db *sql.DB
+	var err error
+	if db, err = sql.Open("postgres", dataSourceName); err != nil {
+		return nil, err
+	}
+	a := accountsStatements{}
+	if err = a.prepare(db, serverName); err != nil {
+		return nil, err
+	}
+	return &AccountDatabase{db, a}, nil
+}
+
+// GetAccountByPassword returns the account associated with the given localpart and password.
+// Returns sql.ErrNoRows if no account exists which matches the given credentials.
+func (d *AccountDatabase) GetAccountByPassword(localpart, plaintextPassword string) (*types.Account, error) {
+	hash, err := hashPassword(plaintextPassword)
+	if err != nil {
+		return nil, err
+	}
+	return d.accounts.selectAccountByPasswordHash(localpart, hash)
+}
+
+// CreateAccount makes a new account with the given login name and password. If no password is supplied,
+// the account will be a passwordless account.
+func (d *AccountDatabase) CreateAccount(localpart, plaintextPassword string) (*types.Account, error) {
+	hash, err := hashPassword(plaintextPassword)
+	if err != nil {
+		return nil, err
+	}
+	return d.accounts.insertAccount(localpart, hash)
+}
+
+func hashPassword(plaintext string) (hash string, err error) {
+	hashBytes, err := bcrypt.GenerateFromPassword([]byte(plaintext), bcrypt.DefaultCost)
+	return string(hashBytes), err
+}
