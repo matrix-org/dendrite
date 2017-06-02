4b4baa079c0884bff931e1a9b303f7d058394f60
Mark Haines <mjark@negativecurvature.net>
2017-06-02 14:17:22 +0100

    Revert "Add API for querying events by ID. (#127)"
    This reverts commit ce82158abb565b31d70628cc2271e3e4bb2fa4be.

--- src/github.com/matrix-org/dendrite/federationapi/routing/routing.go
+++ src/github.com/matrix-org/dendrite/federationapi/routing/routing.go
@@ -16,34 +16,21 @@ package routing
 
 import (
 	"github.com/gorilla/mux"
-	"github.com/matrix-org/dendrite/clientapi/producers"
 	"github.com/matrix-org/dendrite/federationapi/config"
 	"github.com/matrix-org/dendrite/federationapi/readers"
-	"github.com/matrix-org/dendrite/federationapi/writers"
-	"github.com/matrix-org/dendrite/roomserver/api"
-	"github.com/matrix-org/gomatrixserverlib"
 	"github.com/matrix-org/util"
 	"github.com/prometheus/client_golang/prometheus"
 	"net/http"
-	"time"
 )
 
 const (
-	pathPrefixV2Keys       = "/_matrix/key/v2"
-	pathPrefixV1Federation = "/_matrix/federation/v1"
+	pathPrefixV2Keys = "/_matrix/key/v2"
 )
 
 // Setup registers HTTP handlers with the given ServeMux.
-func Setup(
-	servMux *http.ServeMux,
-	cfg config.FederationAPI,
-	query api.RoomserverQueryAPI,
-	producer *producers.RoomserverProducer,
-	keys gomatrixserverlib.KeyRing,
-) {
+func Setup(servMux *http.ServeMux, cfg config.FederationAPI) {
 	apiMux := mux.NewRouter()
 	v2keysmux := apiMux.PathPrefix(pathPrefixV2Keys).Subrouter()
-	v1fedmux := apiMux.PathPrefix(pathPrefixV1Federation).Subrouter()
 
 	localKeys := makeAPI("localkeys", func(req *http.Request) util.JSONResponse {
 		return readers.LocalKeys(req, cfg)
@@ -55,17 +42,6 @@ func Setup(
 	// {keyID} argument and always return a response containing all of the keys.
 	v2keysmux.Handle("/server/{keyID}", localKeys)
 	v2keysmux.Handle("/server/", localKeys)
-
-	v1fedmux.Handle("/send/{txnID}/", makeAPI("send",
-		func(req *http.Request) util.JSONResponse {
-			vars := mux.Vars(req)
-			return writers.Send(
-				req, gomatrixserverlib.TransactionID(vars["txnID"]),
-				time.Now(),
-				cfg, query, producer, keys,
-			)
-		},
-	))
 
 	servMux.Handle("/metrics", prometheus.Handler())
 	servMux.Handle("/api/", http.StripPrefix("/api", apiMux))
