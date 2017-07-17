4eec8da6b504e2b14f7c95e5177dae00010473e3
Mark Haines <mjark@negativecurvature.net>
2017-07-17 14:15:07 +0100

    Remove unused config for kafka topic

--- src/github.com/matrix-org/dendrite/roomserver/storage/storage.go
+++ src/github.com/matrix-org/dendrite/roomserver/storage/storage.go
@@ -18,7 +18,6 @@ import (
 	"database/sql"
 	// Import the postgres database driver.
 	_ "github.com/lib/pq"
-	"github.com/matrix-org/dendrite/common"
 	"github.com/matrix-org/dendrite/roomserver/types"
 	"github.com/matrix-org/gomatrixserverlib"
 )
@@ -40,16 +39,6 @@ func Open(dataSourceName string) (*Database, error) {
 		return nil, err
 	}
 	return &d, nil
-}
-
-// PartitionOffsets implements input.ConsumerDatabase
-func (d *Database) PartitionOffsets(topic string) ([]common.PartitionOffset, error) {
-	return d.statements.SelectPartitionOffsets(topic)
-}
-
-// SetPartitionOffset implements input.ConsumerDatabase
-func (d *Database) SetPartitionOffset(topic string, partition int32, offset int64) error {
-	return d.statements.UpsertPartitionOffset(topic, partition, offset)
 }
 
 // StoreEvent implements input.EventDatabase
