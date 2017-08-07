d027d9b9a26e9b2a97d19208a3101a5d51e7a539
Brendan Abolivier <contact@brendanabolivier.com>
2017-08-07 13:06:55 +0100

    Add some log so we know the initialisation went well

--- src/github.com/matrix-org/dendrite/cmd/dendrite-monolith-server/main.go
+++ src/github.com/matrix-org/dendrite/cmd/dendrite-monolith-server/main.go
@@ -88,11 +88,13 @@ func main() {
 
 	// Expose the matrix APIs directly rather than putting them under a /api path.
 	go func() {
+		log.Info("Listening on ", *httpBindAddr)
 		log.Fatal(http.ListenAndServe(*httpBindAddr, m.api))
 	}()
 	// Handle HTTPS if certificate and key are provided
 	go func() {
 		if *certFile != "" && *keyFile != "" {
+			log.Info("Listening on ", *httpsBindAddr)
 			log.Fatal(http.ListenAndServeTLS(*httpsBindAddr, *certFile, *keyFile, m.api))
 		}
 	}()
