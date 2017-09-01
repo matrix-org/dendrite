1b0795f5415cbbbe7282d9b498a5c4b428804c94
Richard van der Hoff <richard@matrix.org>
2017-09-01 11:11:29 +0100

    Add scripts to run sytest on jenkins

---
+++ jenkins/test-monolith.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+set -eux
+
+cd `dirname $0`/..
+
+: ${WORKSPACE:="$(pwd)"}
+export WORKSPACE
+
+./jenkins/prepare-dendrite.sh
+
+if [ ! -d "sytest" ]; then
+    git clone https://github.com/matrix-org/sytest.git --depth 1 --branch dendrite
+else
+    git -C sytest fetch --depth 1 origin dendrite
+    git -C sytest reset --hard FETCH_HEAD
+fi
+
+./sytest/jenkins/prep_sytest_for_postgres.sh
+
+./sytest/jenkins/install_and_run.sh \
+    -I Dendrite::Monolith \
+    --dendrite-binary-directory $WORKSPACE/bin
