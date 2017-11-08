51b111bc5f20a884b5dd9108968a801e05b8aff1
Erik Johnston <erik@matrix.org>
2017-11-08 10:39:15 +0000

    Move cert creation into script

--- scripts/travis-test.sh
+++ scripts/travis-test.sh
@@ -42,6 +42,9 @@ if [ "${TEST_SUITE:-integ-test}" == "integ-test" ]; then
     gb build github.com/matrix-org/dendrite/cmd/mediaapi-integration-tests
     gb build github.com/matrix-org/dendrite/cmd/client-api-proxy
 
+    # Create necessary certificates and keys to run dendrite
+    openssl req -x509 -newkey rsa:512 -keyout server.key -out server.crt -days 365 -nodes -subj /CN=localhost
+
     # Run the integration tests
     bin/roomserver-integration-tests
     bin/syncserver-integration-tests
