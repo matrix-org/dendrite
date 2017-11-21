1894d6e8e3eae64505bb7272a26322225e442f8c
Erik Johnston <erik@matrix.org>
2017-11-21 11:53:34 +0000

    Update path

--- scripts/install-local-kafka.sh
+++ scripts/install-local-kafka.sh
@@ -5,7 +5,7 @@
 set -eu
 
 # The mirror to download kafka from is picked from the list of mirrors at
-# https://www.apache.org/dyn/closer.cgi?path=/kafka/0.10.2.0/kafka_2.11-0.10.2.0.tgz
+# https://www.apache.org/dyn/closer.cgi?path=/kafka/0.10.2.0/kafka_2.11-0.11.0.2.tgz
 # TODO: Check the signature since we are downloading over HTTP.
 MIRROR=http://apache.mirror.anlx.net/kafka/0.11.0.2/kafka_2.11-0.11.0.2.tgz
 
