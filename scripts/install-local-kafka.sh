7e2362cd2eb5081f449fff90f52efe02d676d5d7
Richard van der Hoff <github@rvanderhoff.org.uk>
2017-12-19 14:09:15 +0000

    Make travis builds a bit faster (#382)
    * travis: clone depth=1
    * cache kafka download

--- scripts/install-local-kafka.sh
+++ scripts/install-local-kafka.sh
@@ -4,15 +4,19 @@
 
 set -eu
 
+cd `dirname $0`/..
+
+mkdir -p .downloads
+
 # The mirror to download kafka from is picked from the list of mirrors at
 # https://www.apache.org/dyn/closer.cgi?path=/kafka/0.10.2.0/kafka_2.11-0.11.0.2.tgz
 # TODO: Check the signature since we are downloading over HTTP.
 MIRROR=http://apache.mirror.anlx.net/kafka/0.11.0.2/kafka_2.11-0.11.0.2.tgz
 
 # Only download the kafka if it isn't already downloaded.
-test -f kafka.tgz || wget $MIRROR -O kafka.tgz
+test -f .downloads/kafka.tgz || wget $MIRROR -O .downloads/kafka.tgz
 # Unpack the kafka over the top of any existing installation
-mkdir -p kafka && tar xzf kafka.tgz -C kafka --strip-components 1
+mkdir -p kafka && tar xzf .downloads/kafka.tgz -C kafka --strip-components 1
 # Start the zookeeper running in the background.
 # By default the zookeeper listens on localhost:2181
 kafka/bin/zookeeper-server-start.sh -daemon kafka/config/zookeeper.properties
