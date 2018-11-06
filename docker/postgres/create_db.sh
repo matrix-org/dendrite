daf57b19b71b036f35cd3f8e7d399ae04736b0b0
Matthew Hodgson <matthew@arasphere.net>
2018-11-06 18:14:39 +0000

    make docker work on current codebase (#584)

--- docker/postgres/create_db.sh
+++ docker/postgres/create_db.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-for db in account device mediaapi syncapi roomserver serverkey federationsender publicroomsapi naffka; do
-    createdb -O dendrite dendrite_$db
+for db in account device mediaapi syncapi roomserver serverkey federationsender publicroomsapi appservice naffka; do
+    createdb -U dendrite -O dendrite dendrite_$db
 done
