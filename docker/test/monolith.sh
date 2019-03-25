329900f644b71348b4d46ddad811f3f827fe2d35
Maximilian Seifert <max.seifert@drglitch.net>
2019-03-25 19:30:31 +0100

    Add docker-compose file for executing tests
    Signed-off-by: Maximilian Seifert <max.seifert@drglitch.net>

--- docker/test/monolith.sh
+++ docker/test/monolith.sh
@@ -3,7 +3,4 @@
 rm -rf build
 bash ./docker/build.sh
 
-TEST_SUITE=unit-test  scripts/travis-test.sh
-TEST_SUITE=integ-test scripts/travis-test.sh
-TEST_SUITE=lint       scripts/travis-test.sh
-
+gb test
