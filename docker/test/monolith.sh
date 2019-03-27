db50a1d69e1fce770bb3178afa212f00a742f35b
Maximilian Seifert <max.seifert@drglitch.net>
2019-03-27 22:00:21 +0100

    Run travis tests in monolith_test
    Signed-off-by: Maximilian Seifert <max.seifert@drglitch.net>

--- docker/test/monolith.sh
+++ docker/test/monolith.sh
@@ -3,4 +3,7 @@
 rm -rf build
 bash ./docker/build.sh
 
-gb test
+TEST_SUITE=unit-test  scripts/travis-test.sh
+TEST_SUITE=integ-test scripts/travis-test.sh
+TEST_SUITE=lint       scripts/travis-test.sh
+
