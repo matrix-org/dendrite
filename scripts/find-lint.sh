0b540635b7a72b8630c4926fd00dee6939a11b4c
Andrew Morgan <andrew@amorgan.xyz>
2019-05-29 16:40:44 +0100

    test

--- scripts/find-lint.sh
+++ scripts/find-lint.sh
@@ -3,10 +3,13 @@
 # Runs the linters against dendrite
 
 # The linters can take a lot of resources and are slow, so they can be
-# configured using two environment variables:
+# configured using the following environment variables:
 #
 # - `DENDRITE_LINT_CONCURRENCY` - number of concurrent linters to run,
 #   golangci-lint defaults this to NumCPU
+# - `GOGC` - how often to perform garbage collection during golangci-lint runs.
+#   Essentially a ratio of memory/speed. See https://github.com/golangci/golangci-lint#memory-usage-of-golangci-lint
+#   for more info.
 
 
 set -eux
