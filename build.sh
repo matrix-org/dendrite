fe72cd8c565b47965e38ff7a98a449c46f2d07eb
Andrew Morgan <andrew@amorgan.xyz>
2019-07-18 15:29:34 +0100

    Use race condition detection on sytest runs

--- build.sh
+++ build.sh
@@ -1,3 +1,25 @@
 #!/bin/sh
 
-GOBIN=$PWD/`dirname $0`/bin go install -v ./cmd/...
+ARGS="-v ./cmd/..."
+
+usage() {
+  echo "Usage: $0 [-r]" 1>&2
+  echo
+  echo "-r"
+  echo "  Build with race condition detection"
+  exit 1
+}
+
+while getopts ":r" o; do
+    case "${o}" in
+        r)
+            # Turn on race condition detection
+            ARGS="-race $ARGS"
+            ;;
+        *)
+            usage
+            ;;
+    esac
+done
+
+GOBIN=$PWD/`dirname $0`/bin go install $ARGS
