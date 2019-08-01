e9f824b958e3eca1c7e16a85d4218c1f57d9e44c
Cnly <minecnly@gmail.com>
2019-08-01 12:25:48 +0800

    Remove unnecessary sanity checks
    Signed-off-by: Alex Chen <minecnly@gmail.com>

--- common/redaction.go
+++ common/redaction.go
@@ -24,16 +24,6 @@ func ValidateRedaction(
 		return true, false, nil
 	}
 
-	// Don't allow an event to redact itself
-	if redaction.Redacts() == redaction.EventID() {
-		return true, false, nil
-	}
-
-	// Don't allow two events to redact each other
-	if redacted.Redacts() == redaction.EventID() {
-		return true, false, nil
-	}
-
 	var expectedDomain, redactorDomain gomatrixserverlib.ServerName
 	if _, expectedDomain, err = gomatrixserverlib.SplitID(
 		'@', redacted.Sender(),
