5c8296236ddbc71e6c7b5a39b969d6099841a99c
Maximilian Seifert <max.seifert@drglitch.net>
2019-08-06 18:31:11 +0200

    Remove superfluous scripts and docker-compose-test
    This MR should now comply with the current environment as Travis-CI is
    no longer in use.
    
    Signed-off-by: Maximilian Seifert <max.seifert@drglitch.net>

--- docker/test/monolith.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-rm -rf build
-bash ./docker/build.sh
-
-TEST_SUITE=unit-test  scripts/travis-test.sh
-TEST_SUITE=integ-test scripts/travis-test.sh
-TEST_SUITE=lint       scripts/travis-test.sh
-
