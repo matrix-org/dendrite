2cb7c91c5d55e39eda0eb1668292c0973b7fa65f
Neil Alexander <neilalexander@users.noreply.github.com>
2020-01-25 14:12:52 +0000

    Store our own keys in the keydb (#853)
    * Store our own keys in the keydb
    
    The DirectKeyFetcher makes the assumption that you can always reach the key/v2/server endpoint of any server, including our own. We previously haven't bothered to store our own keys in the keydb so this would mean we end up making key requests to ourselves.
    
    In the libp2p world as an example, self-dialling is not possible, therefore this would render it impossible to get our own keys.
    
    This commit adds our own keys into the keydb so that we don't create unnecessarily (and maybe impossible) requests.
    
    * Use golang.org/x/crypto/ed25519 instead of crypto/ed25519 for pre-Go 1.13

--- common/keydb/keydb.go
+++ common/keydb/keydb.go
@@ -18,6 +18,8 @@ import (
 	"context"
 	"net/url"
 
+	"golang.org/x/crypto/ed25519"
+
 	"github.com/matrix-org/dendrite/common/keydb/postgres"
 	"github.com/matrix-org/gomatrixserverlib"
 )
@@ -29,15 +31,20 @@ type Database interface {
 }
 
 // NewDatabase opens a database connection.
-func NewDatabase(dataSourceName string) (Database, error) {
+func NewDatabase(
+	dataSourceName string,
+	serverName gomatrixserverlib.ServerName,
+	serverKey ed25519.PublicKey,
+	serverKeyID gomatrixserverlib.KeyID,
+) (Database, error) {
 	uri, err := url.Parse(dataSourceName)
 	if err != nil {
-		return postgres.NewDatabase(dataSourceName)
+		return postgres.NewDatabase(dataSourceName, serverName, serverKey, serverKeyID)
 	}
 	switch uri.Scheme {
 	case "postgres":
-		return postgres.NewDatabase(dataSourceName)
+		return postgres.NewDatabase(dataSourceName, serverName, serverKey, serverKeyID)
 	default:
-		return postgres.NewDatabase(dataSourceName)
+		return postgres.NewDatabase(dataSourceName, serverName, serverKey, serverKeyID)
 	}
 }
