18b652f0fb07f91d05c0f89d42c37b939ba3a8b6
Kegan Dougal <kegan@matrix.org>
2020-03-05 17:00:48 +0000

    Add a no-op main for dendritejs when not building under wasm

--- build.sh
+++ build.sh
@@ -3,8 +3,4 @@
 # Put installed packages into ./bin
 export GOBIN=$PWD/`dirname $0`/bin
 
-# We can't build all packages ala ./cmd/... because ./cmd/dendritejs only works for WASM
-find ./cmd -mindepth 1 -maxdepth 1 -type d | grep -v dendritejs | xargs go install -v
-
-# worth compiling it to make sure it's happy though, and who knows maybe they do want to build for wasm ;)
-GOOS=js GOARCH=wasm go build -o dendritejs.wasm $PWD/`dirname $0`/cmd/dendritejs
+go install -v $PWD/`dirname $0`/cmd/...
