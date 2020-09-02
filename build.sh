3a5cb6637cead23952ca96d9fb940fefac95947e
Neil Alexander <neilalexander@users.noreply.github.com>
2020-09-02 16:10:55 +0100

    Update build.sh

--- build.sh
+++ build.sh
@@ -6,6 +6,8 @@ export GOBIN=$PWD/`dirname $0`/bin
 export BRANCH=`(git symbolic-ref --short HEAD | cut -d'/' -f 3 )|| ""`
 export BUILD=`git rev-parse --short HEAD || ""`
 
-go install -trimpath -ldflags "-X github.com/matrix-org/dendrite/internal.branch=${BRANCH} -X github.com/matrix-org/dendrite/internal.build=${BUILD}" -v $PWD/`dirname $0`/cmd/...
+export FLAGS="-X github.com/matrix-org/dendrite/internal.branch=$BRANCH -X github.com/matrix-org/dendrite/internal.build=$BUILD"
 
-GOOS=js GOARCH=wasm go build -o main.wasm ./cmd/dendritejs
+go install -trimpath -ldflags "$FLAGS" -v $PWD/`dirname $0`/cmd/...
+
+GOOS=js GOARCH=wasm go build -trimpath -ldflags "$FLAGS" -o main.wasm ./cmd/dendritejs
