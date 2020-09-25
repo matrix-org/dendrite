6275669e65538a0f4c0619bc326e0e6558472d0f
Neil Alexander <neilalexander@users.noreply.github.com>
2020-09-25 12:59:57 +0100

    Set default room version to v6 (#1438)

--- roomserver/version/version.go
+++ roomserver/version/version.go
@@ -23,7 +23,7 @@ import (
 // DefaultRoomVersion contains the room version that will, by
 // default, be used to create new rooms on this server.
 func DefaultRoomVersion() gomatrixserverlib.RoomVersion {
-	return gomatrixserverlib.RoomVersionV5
+	return gomatrixserverlib.RoomVersionV6
 }
 
 // RoomVersions returns a map of all known room versions to this
