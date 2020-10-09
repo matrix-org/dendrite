024df5ef39be5a0a352bd3e0734980e717ae6a47
Till Faelligen <tfaelligen@gmail.com>
2020-10-09 13:10:35 +0200

    Use default value when adding column which is not null
    Signed-off-by: Till Faelligen <tfaelligen@gmail.com>

--- userapi/storage/devices/postgres/deltas/20201001204705_last_seen_ts_ip.sql
+++ userapi/storage/devices/postgres/deltas/20201001204705_last_seen_ts_ip.sql
@@ -1,6 +1,6 @@
 -- +goose Up
 -- +goose StatementBegin
-ALTER TABLE device_devices ADD COLUMN IF NOT EXISTS last_seen_ts BIGINT NOT NULL;
+ALTER TABLE device_devices ADD COLUMN IF NOT EXISTS last_seen_ts BIGINT NOT NULL DEFAULT EXTRACT(EPOCH FROM CURRENT_TIMESTAMP)*1000;
 ALTER TABLE device_devices ADD COLUMN IF NOT EXISTS ip TEXT;
 ALTER TABLE device_devices ADD COLUMN IF NOT EXISTS user_agent TEXT;
 -- +goose StatementEnd
