33056062ec77a5ed3579eade13354b15339301be
Neil Alexander <neilalexander@users.noreply.github.com>
2020-10-20 14:07:33 +0100

    Copyright notices etc

--- cmd/dendrite-polylith-multi/main.go
+++ cmd/dendrite-polylith-multi/main.go
@@ -1,3 +1,17 @@
+// Copyright 2020 The Matrix.org Foundation C.I.C.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
 package main
 
 import (
@@ -11,14 +25,13 @@ import (
 	"github.com/sirupsen/logrus"
 )
 
-type starter func(base *setup.BaseDendrite, cfg *config.Dendrite)
+type entrypoint func(base *setup.BaseDendrite, cfg *config.Dendrite)
 
-// nolint:gocyclo
 func main() {
-	flag.Parse()
-
+	cfg := setup.ParseFlags(true)
 	component := flag.Arg(0)
-	components := map[string]starter{
+
+	components := map[string]entrypoint{
 		"appservice":       personalities.Appservice,
 		"clientapi":        personalities.ClientAPI,
 		"eduserver":        personalities.EDUServer,
@@ -46,8 +59,6 @@ func main() {
 	}
 
 	logrus.Infof("Starting %q component", component)
-
-	cfg := setup.ParseFlags(true)
 
 	base := setup.NewBaseDendrite(cfg, component, false) // TODO
 	defer base.Close()                                   // nolint: errcheck
