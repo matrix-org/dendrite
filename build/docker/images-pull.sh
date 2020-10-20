39c7a8915ccba3ac4d281ae7938f2e912ad966b4
Neil Alexander <neilalexander@users.noreply.github.com>
2020-10-20 16:11:24 +0100

    Multi-personality polylith binary (#1552)
    * Initial work oon multipersonality binary
    
    * Remove old binaries
    
    * Monolith and polylith binaries
    
    * Better logging
    
    * dendrite-poly-multi
    
    * Fix path
    
    * Copyright notices etc
    
    * Tweaks
    
    * Update Docker, INSTALL.md
    
    * Take first argument if flags package doesn't find any args
    
    * Postgres 9.6 or later, fix some more Docker stuff
    
    * Don't create unnecessary e2ekey DB
    
    * Run go mod tidy

--- build/docker/images-pull.sh
+++ build/docker/images-pull.sh
@@ -5,15 +5,4 @@ TAG=${1:-latest}
 echo "Pulling tag '${TAG}'"
 
 docker pull matrixdotorg/dendrite-monolith:${TAG}
-
-docker pull matrixdotorg/dendrite-appservice:${TAG}
-docker pull matrixdotorg/dendrite-clientapi:${TAG}
-docker pull matrixdotorg/dendrite-eduserver:${TAG}
-docker pull matrixdotorg/dendrite-federationapi:${TAG}
-docker pull matrixdotorg/dendrite-federationsender:${TAG}
-docker pull matrixdotorg/dendrite-keyserver:${TAG}
-docker pull matrixdotorg/dendrite-mediaapi:${TAG}
-docker pull matrixdotorg/dendrite-roomserver:${TAG}
-docker pull matrixdotorg/dendrite-syncapi:${TAG}
-docker pull matrixdotorg/dendrite-signingkeyserver:${TAG}
-docker pull matrixdotorg/dendrite-userapi:${TAG}
+docker pull matrixdotorg/dendrite-polylith:${TAG}
