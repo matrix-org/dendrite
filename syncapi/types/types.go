add47f5cb859f341e6806b9e43019bc016a95604
Neil Alexander <neilalexander@users.noreply.github.com>
2020-12-02 14:56:29 +0000

    FormatSync instead of FormatAll

--- syncapi/types/types.go
+++ syncapi/types/types.go
@@ -484,7 +484,7 @@ func NewInviteResponse(event *gomatrixserverlib.HeaderedEvent) *InviteResponse {
 
 	// Then we'll see if we can create a partial of the invite event itself.
 	// This is needed for clients to work out *who* sent the invite.
-	inviteEvent := gomatrixserverlib.ToClientEvent(event.Unwrap(), gomatrixserverlib.FormatAll)
+	inviteEvent := gomatrixserverlib.ToClientEvent(event.Unwrap(), gomatrixserverlib.FormatSync)
 	inviteEvent.Unsigned = nil
 	if ev, err := json.Marshal(inviteEvent); err == nil {
 		res.InviteState.Events = append(res.InviteState.Events, ev)
