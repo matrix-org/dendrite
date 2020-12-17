dcab0654cb632f4c83dae571c147329f46980e96
Neil Alexander <neilalexander@users.noreply.github.com>
2020-12-17 15:08:46 +0000

    Revert "Rely on positions from notifier rather than manually advancing them"
    This reverts commit 53112a62cc3bfd9989acab518e69eeb27938117a.

--- syncapi/sync/requestpool.go
+++ syncapi/sync/requestpool.go
@@ -325,6 +325,7 @@ func (rp *RequestPool) currentSyncForUser(req syncRequest, latestPos types.Strea
 		// Add the updates into the sync response.
 		for _, event := range events {
 			res.ToDevice.Events = append(res.ToDevice.Events, event.SendToDeviceEvent)
+			res.NextBatch.SendToDevicePosition++
 		}
 	}
 
