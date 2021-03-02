232420984381920f60f3d0355278c9bee88537aa
Neil Alexander <neilalexander@users.noreply.github.com>
2021-03-02 10:24:31 +0000

    Fix syntax error

--- keyserver/internal/internal.go
+++ keyserver/internal/internal.go
@@ -515,7 +515,7 @@ func (a *KeyInternalAPI) uploadLocalDeviceKeys(ctx context.Context, req *api.Per
 func (a *KeyInternalAPI) uploadOneTimeKeys(ctx context.Context, req *api.PerformUploadKeysRequest, res *api.PerformUploadKeysResponse) {
 	if req.UserID == "" || req.DeviceID == "" {
 		res.Error = &api.KeyError{
-			Err: fmt.Sprintf("user ID or device ID missing")
+			Err: "user ID or device ID missing",
 		}
 	}
 	if len(req.OneTimeKeys) == 0 {
