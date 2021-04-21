c852dc41800d4f4070af8073b368bdb7ade009a9
Piotr Kozimor <p1996k@gmail.com>
2021-04-21 15:10:30 +0200

    Fix tag for Flow struct field

--- clientapi/auth/authtypes/flow.go
+++ clientapi/auth/authtypes/flow.go
@@ -17,5 +17,5 @@ package authtypes
 // Flow represents one possible way that the client can authenticate a request.
 // https://matrix.org/docs/spec/client_server/r0.3.0.html#user-interactive-authentication-api
 type Flow struct {
-	Stages []LoginType `json:"stages" yaml:"stages`
+	Stages []LoginType `json:"stages" yaml:"stages"`
 }
