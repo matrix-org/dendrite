f3997384d5b20ba57e9583b9d744b5f9cdaf09fe
J. Ryan Stinnett <jryans@gmail.com>
2021-07-10 16:02:14 +0100

    Match path style from build.sh

--- build-dendritejs.sh
+++ build-dendritejs.sh
@@ -1,4 +1,4 @@
 #!/bin/sh -eu
 
 export GIT_COMMIT=$(git rev-list -1 HEAD) && \
-GOOS=js GOARCH=wasm go build -ldflags "-X main.GitCommit=$GIT_COMMIT" -o ./bin/main.wasm ./cmd/dendritejs-pinecone
+GOOS=js GOARCH=wasm go build -ldflags "-X main.GitCommit=$GIT_COMMIT" -o bin/main.wasm ./cmd/dendritejs-pinecone
