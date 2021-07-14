ec4e0302c00320b90353aaf0f73a2f613153cfb9
Neil Alexander <neilalexander@users.noreply.github.com>
2021-07-14 14:50:17 +0100

    Disable interest policies

--- setup/jetstream/streams.go
+++ setup/jetstream/streams.go
@@ -17,14 +17,14 @@ var (
 
 var streams = []*nats.StreamConfig{
 	{
-		Name:      OutputRoomEvent,
-		Retention: nats.InterestPolicy,
-		Storage:   nats.FileStorage,
+		Name: OutputRoomEvent,
+		//Retention: nats.InterestPolicy,
+		Storage: nats.FileStorage,
 	},
 	{
-		Name:      OutputSendToDeviceEvent,
-		Retention: nats.InterestPolicy,
-		Storage:   nats.FileStorage,
+		Name: OutputSendToDeviceEvent,
+		//Retention: nats.InterestPolicy,
+		Storage: nats.FileStorage,
 	},
 	{
 		Name:      OutputKeyChangeEvent,
@@ -32,19 +32,19 @@ var streams = []*nats.StreamConfig{
 		Storage:   nats.FileStorage,
 	},
 	{
-		Name:      OutputTypingEvent,
-		Retention: nats.InterestPolicy,
-		Storage:   nats.MemoryStorage,
-		MaxAge:    time.Second * 60,
+		Name: OutputTypingEvent,
+		//Retention: nats.InterestPolicy,
+		Storage: nats.MemoryStorage,
+		MaxAge:  time.Second * 60,
 	},
 	{
-		Name:      OutputClientData,
-		Retention: nats.InterestPolicy,
-		Storage:   nats.FileStorage,
+		Name: OutputClientData,
+		//Retention: nats.InterestPolicy,
+		Storage: nats.FileStorage,
 	},
 	{
-		Name:      OutputReceiptEvent,
-		Retention: nats.InterestPolicy,
-		Storage:   nats.FileStorage,
+		Name: OutputReceiptEvent,
+		//Retention: nats.InterestPolicy,
+		Storage: nats.FileStorage,
 	},
 }
