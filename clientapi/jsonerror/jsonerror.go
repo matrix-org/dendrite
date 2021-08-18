fe5050c0a5b1ef3dddde294d64ab24d8ba26e53f
Kevin Liu <mail@nivekuil.com>
2021-08-18 04:41:30 -0700

    Add and use M_ROOM_IN_USE (#1972)
    Signed-off-by: nivekuil <mail@nivekuil.com>
    
    Co-authored-by: Neil Alexander <neilalexander@users.noreply.github.com>

--- clientapi/jsonerror/jsonerror.go
+++ clientapi/jsonerror/jsonerror.go
@@ -111,6 +111,12 @@ func UserInUse(msg string) *MatrixError {
 	return &MatrixError{"M_USER_IN_USE", msg}
 }
 
+// RoomInUse is an error returned when the client tries to make a room
+// that already exists
+func RoomInUse(msg string) *MatrixError {
+	return &MatrixError{"M_ROOM_IN_USE", msg}
+}
+
 // ASExclusive is an error returned when an application service tries to
 // register an username that is outside of its registered namespace, or if a
 // user attempts to register a username or room alias within an exclusive
