6af0e717297db022febb364b7d586cef7c29548c
Jared Jacobson <jaredj@criticalarc.com>
2021-09-13 11:16:33 +1000

    Modify the build script

--- build.sh
+++ build.sh
@@ -2,20 +2,7 @@
 
 # Put installed packages into ./bin
 export GOBIN=$PWD/`dirname $0`/bin
-
-if [ -d ".git" ] 
-then
-    export BUILD=`git rev-parse --short HEAD || ""`
-    export BRANCH=`(git symbolic-ref --short HEAD | tr -d \/ ) || ""`
-    if [ "$BRANCH" = master ]
-    then
-        export BRANCH=""
-    fi
-
-    export FLAGS="-X github.com/matrix-org/dendrite/internal.branch=$BRANCH -X github.com/matrix-org/dendrite/internal.build=$BUILD"
-else
-    export FLAGS=""
-fi
+export FLAGS=""
 
 mkdir -p bin
 
