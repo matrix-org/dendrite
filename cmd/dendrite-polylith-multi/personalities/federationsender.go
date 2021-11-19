9558d64d9c1c465d4ee984ff78b33f6dd1b684a2
Till Faelligen <tfaelligen@gmail.com>
2021-11-19 23:31:32 +0100

    Add missing file..

--- cmd/dendrite-polylith-multi/personalities/federationsender.go
+++ cmd/dendrite-polylith-multi/personalities/federationsender.go
@@ -28,7 +28,7 @@ func FederationSender(base *setup.BaseDendrite, cfg *config.Dendrite) {
 
 	rsAPI := base.RoomserverHTTPClient()
 	fsAPI := federationsender.NewInternalAPI(
-		base, federation, rsAPI, keyRing, false,
+		base, federation, rsAPI, nil, keyRing, false,
 	)
 	federationsender.AddInternalRoutes(base.InternalAPIMux, fsAPI)
 
