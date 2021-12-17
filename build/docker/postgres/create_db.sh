580a256c3794ee272577232675ab73c9bc9a8178
Neil Alexander <neilalexander@users.noreply.github.com>
2021-12-17 14:20:34 +0000

    More Docker updates

--- build/docker/postgres/create_db.sh
+++ build/docker/postgres/create_db.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
-for db in userapi_accounts userapi_devices mediaapi syncapi roomserver keyserver federationapi appservice naffka; do
+for db in userapi_accounts userapi_devices mediaapi syncapi roomserver keyserver federationapi appservice mscs; do
     createdb -U dendrite -O dendrite dendrite_$db
 done
