378ccaa1c49f047912c99dc98dbe38c1f05fb531
Neil Alexander <neilalexander@users.noreply.github.com>
2022-02-08 10:09:56 +0000

    Only un-reject, don't re-reject

--- roomserver/storage/postgres/events_table.go
+++ roomserver/storage/postgres/events_table.go
@@ -74,7 +74,7 @@ const insertEventSQL = "" +
 	"INSERT INTO roomserver_events (room_nid, event_type_nid, event_state_key_nid, event_id, reference_sha256, auth_event_nids, depth, is_rejected)" +
 	" VALUES ($1, $2, $3, $4, $5, $6, $7, $8)" +
 	" ON CONFLICT ON CONSTRAINT roomserver_event_id_unique DO UPDATE" +
-	" SET is_rejected = $8" +
+	" SET is_rejected = $8 WHERE is_rejected = FALSE" +
 	" RETURNING event_nid, state_snapshot_nid"
 
 const selectEventSQL = "" +
