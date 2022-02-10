9f7244f884ab00df83fca70838a695cf7ee8af6c
Till Faelligen <tfaelligen@gmail.com>
2022-02-10 15:49:08 +0100

    Rename migration, add missing update for appservices

--- userapi/storage/accounts/postgres/deltas/2022021013023800_add_account_type.go
+++ userapi/storage/accounts/postgres/deltas/2022021013023800_add_account_type.go
@@ -4,14 +4,8 @@ import (
 	"database/sql"
 	"fmt"
 
-	"github.com/pressly/goose"
-
 	"github.com/matrix-org/dendrite/internal/sqlutil"
 )
-
-func init() {
-	goose.AddMigration(UpAddAccountType, DownAddAccountType)
-}
 
 func LoadAddAccountType(m *sqlutil.Migrations) {
 	m.AddMigration(UpAddAccountType, DownAddAccountType)
@@ -19,6 +13,10 @@ func LoadAddAccountType(m *sqlutil.Migrations) {
 
 func UpAddAccountType(tx *sql.Tx) error {
 	_, err := tx.Exec("ALTER TABLE account_accounts ADD COLUMN IF NOT EXISTS account_type INT DEFAULT 2;")
+	if err != nil {
+		return fmt.Errorf("failed to execute upgrade: %w", err)
+	}
+	_, err = tx.Exec("UPDATE account_accounts SET account_type = 4 WHERE appservice_id <> '';")
 	if err != nil {
 		return fmt.Errorf("failed to execute upgrade: %w", err)
 	}
