fe7043588c8499e76a5d5229b48032bd41e5fcb3
Till Faelligen <tfaelligen@gmail.com>
2022-04-07 15:17:22 +0200

    ADd build constraint

--- userapi/util/stats.go
+++ userapi/util/stats.go
@@ -12,8 +12,8 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-//go:build !wasm
-// +build !wasm
+//go:build !wasm && !windows
+// +build !wasm,!windows
 
 package util
 
