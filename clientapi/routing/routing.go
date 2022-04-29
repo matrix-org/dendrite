2a4517f8e6c7f68d8bd94f0b4640b142f2d3ac52
Neil Alexander <neilalexander@users.noreply.github.com>
2022-04-29 09:10:08 +0100

    Move admin functions into their own file in the client API

--- clientapi/routing/routing.go
+++ clientapi/routing/routing.go
@@ -122,40 +122,7 @@ func Setup(
 
 	dendriteAdminRouter.Handle("/admin/evacuateRoom/{roomID}",
 		httputil.MakeAuthAPI("admin_evacuate_room", userAPI, func(req *http.Request, device *userapi.Device) util.JSONResponse {
-			if device.AccountType != userapi.AccountTypeAdmin {
-				return util.JSONResponse{
-					Code: http.StatusForbidden,
-					JSON: jsonerror.Forbidden("This API can only be used by admin users."),
-				}
-			}
-			vars, err := httputil.URLDecodeMapValues(mux.Vars(req))
-			if err != nil {
-				return util.ErrorResponse(err)
-			}
-			roomID, ok := vars["roomID"]
-			if !ok {
-				return util.JSONResponse{
-					Code: http.StatusBadRequest,
-					JSON: jsonerror.MissingArgument("Expecting room ID."),
-				}
-			}
-			res := &roomserverAPI.PerformAdminEvacuateRoomResponse{}
-			rsAPI.PerformAdminEvacuateRoom(
-				req.Context(),
-				&roomserverAPI.PerformAdminEvacuateRoomRequest{
-					RoomID: roomID,
-				},
-				res,
-			)
-			if err := res.Error; err != nil {
-				return err.JSONResponse()
-			}
-			return util.JSONResponse{
-				Code: 200,
-				JSON: map[string]interface{}{
-					"affected": res.Affected,
-				},
-			}
+			return AdminEvacuateRoom(req, device, rsAPI)
 		}),
 	).Methods(http.MethodGet, http.MethodOptions)
 
