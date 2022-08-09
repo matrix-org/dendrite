cc98ffb36265aaeb9cd5ce7724eb5444e9f14d6a
Tak Wai Wong <64229756+tak-hntlabs@users.noreply.github.com>
2022-08-09 15:23:56 -0700

    Tak/pull-dendrite-changes (#245)
    * Fix issues with migrations not getting executed (#2628)
    
    * Fix issues with migrations not getting executed
    
    * Check actual postgres error
    
    * Return error if it's not "column does not exist"
    
    * Remove nonce generation for eip4361 signin (#25)
    
    Co-authored-by: Tak Wai Wong <tak@hntlabs.com>

--- setup/config/config_publickey.go
+++ setup/config/config_publickey.go
@@ -1,35 +1,23 @@
 package config
 
 import (
-	"math/rand"
-	"time"
-
 	"github.com/matrix-org/dendrite/clientapi/auth/authtypes"
 )
 
-var nonceLength = 32
-
 type AuthParams interface {
 	GetParams() interface{}
-	GetNonce() string
 }
 
 type EthereumAuthParams struct {
-	Version  uint   `json:"version"`
-	ChainIDs []int  `json:"chain_ids"`
-	Nonce    string `json:"nonce"`
+	Version  uint  `json:"version"`
+	ChainIDs []int `json:"chain_ids"`
 }
 
 func (p EthereumAuthParams) GetParams() interface{} {
 	copyP := p
 	copyP.ChainIDs = make([]int, len(p.ChainIDs))
 	copy(copyP.ChainIDs, p.ChainIDs)
-	copyP.Nonce = newNonce(nonceLength)
 	return copyP
-}
-
-func (p EthereumAuthParams) GetNonce() string {
-	return p.Nonce
 }
 
 type EthereumAuthConfig struct {
@@ -61,23 +49,9 @@ func (pk *PublicKeyAuthentication) GetPublicKeyRegistrationParams() map[string]i
 		p := EthereumAuthParams{
 			Version:  pk.Ethereum.Version,
 			ChainIDs: pk.Ethereum.ChainIDs,
-			Nonce:    "",
 		}
 		params[authtypes.LoginTypePublicKeyEthereum] = p
 	}
 
 	return params
-}
-
-const lettersAndNumbers = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789"
-
-func newNonce(n int) string {
-	nonce := make([]byte, n)
-	rand.Seed(time.Now().UnixNano())
-
-	for i := range nonce {
-		nonce[i] = lettersAndNumbers[rand.Int63()%int64(len(lettersAndNumbers))]
-	}
-
-	return string(nonce)
 }
