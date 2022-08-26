84b7cf0eac82779e99d6be16088d664d5417fc3e
Neil Alexander <neilalexander@users.noreply.github.com>
2022-08-26 09:34:18 +0100

    Restrict username length properly (fixes #2674)

--- clientapi/routing/routing.go
+++ clientapi/routing/routing.go
@@ -133,7 +133,7 @@ func Setup(
 					}
 				}
 				if req.Method == http.MethodPost {
-					return handleSharedSecretRegistration(userAPI, sr, req)
+					return handleSharedSecretRegistration(cfg, userAPI, sr, req)
 				}
 				return util.JSONResponse{
 					Code: http.StatusMethodNotAllowed,
