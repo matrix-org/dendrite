973f949304fdcb2f9ee9ae0e3f98d01f749020fb
Brian Meek <brian@hntlabs.com>
2022-09-02 12:45:43 -0700

    Update dependencies to see if it helps make tests more reliable, easier to debug
    Signed-off-by: Brian Meek <brian@hntlabs.com>

--- cmd/dendrite-upgrade-tests/main.go
+++ cmd/dendrite-upgrade-tests/main.go
@@ -49,7 +49,7 @@ const HEAD = "HEAD"
 // due to the error:
 // When using COPY with more than one source file, the destination must be a directory and end with a /
 // We need to run a postgres anyway, so use the dockerfile associated with Complement instead.
-const Dockerfile = `FROM golang:1.18-stretch as build
+const Dockerfile = `FROM golang:1.19-buster as build
 RUN apt-get update && apt-get install -y postgresql
 WORKDIR /build
 
@@ -57,25 +57,25 @@ WORKDIR /build
 # Complement Dockerfile which wgets a branch.
 COPY . .
 
-RUN go build ./cmd/dendrite-monolith-server
-RUN go build ./cmd/generate-keys
-RUN go build ./cmd/generate-config
+RUN go build --race ./cmd/dendrite-monolith-server
+RUN go build --race ./cmd/generate-keys
+RUN go build --race ./cmd/generate-config
 RUN ./generate-config --ci > dendrite.yaml
 RUN ./generate-keys --private-key matrix_key.pem --tls-cert server.crt --tls-key server.key
 
 # Replace the connection string with a single postgres DB, using user/db = 'postgres' and no password
 RUN sed -i "s%connection_string:.*$%connection_string: postgresql://postgres@localhost/postgres?sslmode=disable%g" dendrite.yaml 
 # No password when connecting over localhost
-RUN sed -i "s%127.0.0.1/32            md5%127.0.0.1/32            trust%g" /etc/postgresql/9.6/main/pg_hba.conf
+RUN sed -i "s%127.0.0.1/32            md5%127.0.0.1/32            trust%g" /etc/postgresql/11/main/pg_hba.conf
 # Bump up max conns for moar concurrency
-RUN sed -i 's/max_connections = 100/max_connections = 2000/g' /etc/postgresql/9.6/main/postgresql.conf
+RUN sed -i 's/max_connections = 100/max_connections = 2000/g' /etc/postgresql/11/main/postgresql.conf
 RUN sed -i 's/max_open_conns:.*$/max_open_conns: 100/g' dendrite.yaml
 
 # This entry script starts postgres, waits for it to be up then starts dendrite
 RUN echo '\
 #!/bin/bash -eu \n\
 pg_lsclusters \n\
-pg_ctlcluster 9.6 main start \n\
+pg_ctlcluster 11 main start \n\
  \n\
 until pg_isready \n\
 do \n\
@@ -341,7 +341,7 @@ func runImage(dockerClient *client.Client, volumeName, version, imageID string) 
 			{
 				Type:   mount.TypeVolume,
 				Source: volumeName,
-				Target: "/var/lib/postgresql/9.6/main",
+				Target: "/var/lib/postgresql/11/main",
 			},
 		},
 	}, nil, nil, "dendrite_upgrade_test_"+version)
