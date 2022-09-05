5992b4c7ed2779e12c8b9da7bc96b2b6398cf04c
Neil Alexander <neilalexander@users.noreply.github.com>
2022-09-05 17:35:43 +0100

    Set Sentry user where known

--- internal/httputil/httpapi.go
+++ internal/httputil/httpapi.go
@@ -60,7 +60,10 @@ func MakeAuthAPI(
 		// add the user to Sentry, if enabled
 		hub := sentry.GetHubFromContext(req.Context())
 		if hub != nil {
-			hub.Scope().SetTag("username", device.UserID)
+			hub.Scope().SetUser(sentry.User{
+				Username:  device.UserID,
+				IPAddress: req.RemoteAddr,
+			})
 			hub.Scope().SetTag("user_id", device.UserID)
 			hub.Scope().SetTag("device_id", device.ID)
 		}
