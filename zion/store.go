b4223a1a1833931111a2c8ad678803a7897f2489
John Terzis <john@hntlabs.com>
2022-11-01 23:17:48 -0700

    Jterzis/sync dendrite (#793)

--- zion/store.go
+++ zion/store.go
@@ -12,17 +12,19 @@ import (
 	"github.com/matrix-org/gomatrixserverlib"
 )
 
-type Store struct {
+type ClientRoomserverStore struct {
 	rsAPI roomserver.ClientRoomserverAPI
 }
 
-func NewStore(rsAPI roomserver.ClientRoomserverAPI) Store {
-	return Store{
-		rsAPI: rsAPI,
-	}
+type SyncRoomserverStore struct {
+	rsAPI roomserver.SyncRoomserverAPI
 }
 
-func (s *Store) GetRoomInfo(roomId string, userId UserIdentifier) RoomInfo {
+type StoreAPI interface {
+	GetRoomInfo(roomId string, userId UserIdentifier) RoomInfo
+}
+
+func (s *ClientRoomserverStore) GetRoomInfo(roomId string, userId UserIdentifier) RoomInfo {
 	result := RoomInfo{
 		QueryUserId:      userId.MatrixUserId,
 		SpaceNetworkId:   "",
@@ -60,7 +62,7 @@ func (s *Store) GetRoomInfo(roomId string, userId UserIdentifier) RoomInfo {
 	if err != nil {
 		return result
 	}
-
+	//TODO: replace with HydrateRoomInfoWithStateEvents when you have a practical way to flatten roomEvents map
 	for _, state := range roomEvents.StateEvents {
 		switch state.Type() {
 		case gomatrixserverlib.MRoomCreate:
@@ -85,4 +87,86 @@ func (s *Store) GetRoomInfo(roomId string, userId UserIdentifier) RoomInfo {
 	}
 
 	return result
+}
+
+func (s *SyncRoomserverStore) GetRoomInfo(roomId string, userId UserIdentifier) RoomInfo {
+	result := RoomInfo{
+		QueryUserId:      userId.MatrixUserId,
+		SpaceNetworkId:   "",
+		ChannelNetworkId: "",
+		RoomType:         Unknown,
+		IsOwner:          false,
+	}
+
+	createTuple := gomatrixserverlib.StateKeyTuple{
+		EventType: gomatrixserverlib.MRoomCreate,
+		StateKey:  "",
+	}
+
+	spaceChildTuple := gomatrixserverlib.StateKeyTuple{
+		EventType: ConstSpaceChildEventType,
+		StateKey:  "*",
+	}
+
+	spaceParentTuple := gomatrixserverlib.StateKeyTuple{
+		EventType: ConstSpaceParentEventType,
+		StateKey:  "*",
+	}
+
+	var roomEvents roomserver.QueryLatestEventsAndStateResponse
+	err := s.rsAPI.QueryLatestEventsAndState(context.Background(),
+		&roomserver.QueryLatestEventsAndStateRequest{
+			RoomID: roomId,
+			StateToFetch: []gomatrixserverlib.StateKeyTuple{
+				createTuple,
+				spaceParentTuple,
+				spaceChildTuple,
+			},
+		}, &roomEvents)
+
+	if err != nil {
+		return result
+	}
+
+	HydrateRoomInfoWithStateEvents(roomId, userId, &result, roomEvents.StateEvents)
+
+	return result
+}
+
+func HydrateRoomInfoWithStateEvents(roomId string, userId UserIdentifier, r *RoomInfo, stateEvents []*gomatrixserverlib.HeaderedEvent) {
+	for _, state := range stateEvents {
+		switch state.Type() {
+		case gomatrixserverlib.MRoomCreate:
+			var creatorEvent CreatorEvent
+			err := json.Unmarshal(state.Content(), &creatorEvent)
+			r.IsOwner = strings.HasPrefix(
+				creatorEvent.Creator,
+				userId.LocalPart,
+			)
+			if err == nil {
+				r.RoomType = Space
+				r.SpaceNetworkId = roomId
+			}
+		case ConstSpaceChildEventType:
+			r.RoomType = Space
+			r.SpaceNetworkId = roomId
+		case ConstSpaceParentEventType:
+			r.RoomType = Channel
+			r.SpaceNetworkId = *state.StateKey()
+			r.ChannelNetworkId = roomId
+		}
+	}
+
+}
+
+func NewClientRoomserverStore(rsAPI roomserver.ClientRoomserverAPI) StoreAPI {
+	return &ClientRoomserverStore{
+		rsAPI: rsAPI,
+	}
+}
+
+func NewSyncRoomserverStore(rsAPI roomserver.SyncRoomserverAPI) StoreAPI {
+	return &SyncRoomserverStore{
+		rsAPI: rsAPI,
+	}
 }
