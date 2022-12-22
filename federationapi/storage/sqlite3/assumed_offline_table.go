6c47069b0bebd6e57cc57682a1a529b015d332b2
Devon Hudson <devonhudson@librem.one>
2022-12-22 09:47:26 -0700

    Remove duplicate db statement prepares

--- federationapi/storage/sqlite3/assumed_offline_table.go
+++ federationapi/storage/sqlite3/assumed_offline_table.go
@@ -60,19 +60,6 @@ func NewSQLiteAssumedOfflineTable(db *sql.DB) (s *assumedOfflineStatements, err 
 		return
 	}
 
-	if s.insertAssumedOfflineStmt, err = db.Prepare(insertAssumedOfflineSQL); err != nil {
-		return
-	}
-	if s.selectAssumedOfflineStmt, err = db.Prepare(selectAssumedOfflineSQL); err != nil {
-		return
-	}
-	if s.deleteAssumedOfflineStmt, err = db.Prepare(deleteAssumedOfflineSQL); err != nil {
-		return
-	}
-	if s.deleteAllAssumedOfflineStmt, err = db.Prepare(deleteAllAssumedOfflineSQL); err != nil {
-		return
-	}
-
 	return s, sqlutil.StatementList{
 		{&s.insertAssumedOfflineStmt, insertAssumedOfflineSQL},
 		{&s.selectAssumedOfflineStmt, selectAssumedOfflineSQL},
