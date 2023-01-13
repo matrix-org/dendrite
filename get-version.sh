bf303908fa486dd9d23cee7cfa9906daf80038ad
Kerem Kazan <kerem.kazan@gmail.com>
2023-01-13 15:39:39 -0500

    dendrite version from commit hash (#1221)
    this pr computes dendrite docker version from the commit hash, instead
    of bumping versions. the previous setup caused issues and conflicts when
    branch protection is enabled. this way we can seamlessly keep pushing to
    main and it will keep getting built and published

--- get-version.sh
+++ get-version.sh
@@ -1,4 +1,3 @@
 #!/usr/bin/env bash
 
-# This script is used to extract the version string from the package.json file
-echo "$(cat package.json | grep version | head -1 | awk -F: '{ print $2 }' | sed 's/[",]//g' | tr -d '[[:space:]]')" 
+echo $(git log -1 --pretty=%h)
