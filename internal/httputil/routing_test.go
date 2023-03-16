9ac1a924ebbf2738506c9b7e62f798485d4e8b67
Till Faelligen <2353100+S7evinK@users.noreply.github.com>
2023-03-16 13:10:48 +0100

    Basic error tests

---
+++ internal/httputil/routing_test.go
@@ -0,0 +1,38 @@
+package httputil
+
+import (
+	"net/http"
+	"net/http/httptest"
+	"path/filepath"
+	"testing"
+)
+
+func TestRoutersError(t *testing.T) {
+	r := NewRouters()
+
+	// not found test
+	rec := httptest.NewRecorder()
+	req := httptest.NewRequest(http.MethodGet, filepath.Join(PublicFederationPathPrefix, "doesnotexist"), nil)
+	r.Federation.ServeHTTP(rec, req)
+	if rec.Code != http.StatusNotFound {
+		t.Fatalf("unexpected status code: %d - %s", rec.Code, rec.Body.String())
+	}
+	if ct := rec.Header().Get("Content-Type"); ct != "application/json" {
+		t.Logf("unexpected content-type: %s", ct)
+	}
+
+	// not allowed test
+	r.DendriteAdmin.
+		Handle("/test", http.HandlerFunc(func(writer http.ResponseWriter, request *http.Request) {})).
+		Methods(http.MethodPost)
+
+	rec = httptest.NewRecorder()
+	req = httptest.NewRequest(http.MethodGet, filepath.Join(DendriteAdminPathPrefix, "test"), nil)
+	r.DendriteAdmin.ServeHTTP(rec, req)
+	if rec.Code != http.StatusMethodNotAllowed {
+		t.Fatalf("unexpected status code: %d - %s", rec.Code, rec.Body.String())
+	}
+	if ct := rec.Header().Get("Content-Type"); ct != "application/json" {
+		t.Logf("unexpected content-type: %s", ct)
+	}
+}
