1020f604e817855b4663fef9063f338711da14c5
KuhnChris <kuhnchris@kuhnchris.eu>
2023-05-07 20:56:20 +0200

    linting

--- clientapi/auth/login_test.go
+++ clientapi/auth/login_test.go
@@ -19,7 +19,6 @@ import (
 	"net/http"
 	"net/http/httptest"
 	"reflect"
-	"regexp"
 	"strings"
 	"testing"
 
--- clientapi/routing/sendevent.go
+++ clientapi/routing/sendevent.go
@@ -199,7 +199,7 @@ func SendEvent(
 		req.Context(), rsAPI,
 		api.KindNew,
 		[]*types.HeaderedEvent{
-			&types.HeaderedEvent{PDU: e},
+			{PDU: e},
 		},
 		device.UserDomain(),
 		domain,
