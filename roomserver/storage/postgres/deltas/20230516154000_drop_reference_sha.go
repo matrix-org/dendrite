5c699360636d77ea2c298f3c94df9216f6507d23
Till Faelligen <2353100+S7evinK@users.noreply.github.com>
2023-05-26 09:22:43 +0200

    Add tests and fix issue

--- roomserver/storage/postgres/deltas/20230516154000_drop_reference_sha.go
+++ roomserver/storage/postgres/deltas/20230516154000_drop_reference_sha.go
@@ -18,19 +18,15 @@ import (
 	"context"
 	"database/sql"
 	"fmt"
+	"sort"
+
+	"github.com/lib/pq"
+	"github.com/matrix-org/dendrite/internal"
+	"golang.org/x/exp/slices"
 )
 
 func UpDropEventReferenceSHAEvents(ctx context.Context, tx *sql.Tx) error {
-	var count int
-	err := tx.QueryRowContext(ctx, `SELECT count(*) FROM roomserver_events GROUP BY event_id HAVING count(event_id) > 1`).
-		Scan(&count)
-	if err != nil && err != sql.ErrNoRows {
-		return fmt.Errorf("failed to query duplicate event ids")
-	}
-	if count > 0 {
-		return fmt.Errorf("unable to drop column, as there are duplicate event ids")
-	}
-	_, err = tx.ExecContext(ctx, `ALTER TABLE roomserver_events DROP COLUMN IF EXISTS reference_sha256;`)
+	_, err := tx.ExecContext(ctx, `ALTER TABLE roomserver_events DROP COLUMN IF EXISTS reference_sha256;`)
 	if err != nil {
 		return fmt.Errorf("failed to execute upgrade: %w", err)
 	}
@@ -46,9 +42,81 @@ func UpDropEventReferenceSHAPrevEvents(ctx context.Context, tx *sql.Tx) error {
 	if err != nil {
 		return fmt.Errorf("failed to execute upgrade: %w", err)
 	}
+
+	// figure out if there are duplicates
+	dupeRows, err := tx.QueryContext(ctx, `SELECT previous_event_id FROM roomserver_previous_events GROUP BY previous_event_id HAVING count(previous_event_id) > 1`)
+	if err != nil {
+		return fmt.Errorf("failed to query duplicate event ids")
+	}
+	defer internal.CloseAndLogIfError(ctx, dupeRows, "failed to close rows")
+
+	var prevEvents []string
+	var prevEventID string
+	for dupeRows.Next() {
+		if err = dupeRows.Scan(&prevEventID); err != nil {
+			return err
+		}
+		prevEvents = append(prevEvents, prevEventID)
+	}
+	if dupeRows.Err() != nil {
+		return dupeRows.Err()
+	}
+
+	// if we found duplicates, check if we can combine them, e.g. they are in the same room
+	for _, dupeID := range prevEvents {
+		var dupeNIDsRows *sql.Rows
+		dupeNIDsRows, err = tx.QueryContext(ctx, `SELECT event_nids FROM roomserver_previous_events WHERE previous_event_id = $1`, dupeID)
+		if err != nil {
+			return fmt.Errorf("failed to query duplicate event ids")
+		}
+		defer internal.CloseAndLogIfError(ctx, dupeNIDsRows, "failed to close rows")
+		var dupeNIDs []int64
+		for dupeNIDsRows.Next() {
+			var nids pq.Int64Array
+			if err = dupeNIDsRows.Scan(&nids); err != nil {
+				return err
+			}
+			dupeNIDs = append(dupeNIDs, nids...)
+		}
+
+		if dupeNIDsRows.Err() != nil {
+			return dupeNIDsRows.Err()
+		}
+		// dedupe NIDs
+		sort.Sort(nids(dupeNIDs))
+		dupeNIDs = slices.Compact(dupeNIDs)
+		// now that we have all NIDs, check which room they belong to
+		var roomCount int
+		err = tx.QueryRowContext(ctx, `SELECT count(distinct room_nid) FROM roomserver_events WHERE event_nid = ANY($1)`, pq.Array(dupeNIDs)).Scan(&roomCount)
+		if err != nil {
+			return err
+		}
+		// if the events are from different rooms, that's bad and we can't continue
+		if roomCount > 1 {
+			return fmt.Errorf("detected events (%v) referenced for different rooms (%v)", dupeNIDs, roomCount)
+		}
+		// otherwise delete the dupes
+		_, err = tx.ExecContext(ctx, "DELETE FROM roomserver_previous_events WHERE previous_event_id = $1", dupeID)
+		if err != nil {
+			return fmt.Errorf("unable to delete duplicates: %w", err)
+		}
+
+		// insert combined values
+		_, err = tx.ExecContext(ctx, "INSERT INTO roomserver_previous_events (previous_event_id, event_nids) VALUES ($1, $2)", dupeID, pq.Array(dupeNIDs))
+		if err != nil {
+			return fmt.Errorf("unable to insert new event NIDs: %w", err)
+		}
+	}
+
 	_, err = tx.ExecContext(ctx, `ALTER TABLE roomserver_previous_events ADD CONSTRAINT roomserver_previous_event_id_unique UNIQUE (previous_event_id);`)
 	if err != nil {
 		return fmt.Errorf("failed to execute upgrade: %w", err)
 	}
 	return nil
 }
+
+type nids []int64
+
+func (s nids) Len() int           { return len(s) }
+func (s nids) Less(i, j int) bool { return s[i] < s[j] }
+func (s nids) Swap(i, j int)      { s[i], s[j] = s[j], s[i] }
--- roomserver/storage/sqlite3/deltas/20230516154000_drop_reference_sha.go
+++ roomserver/storage/sqlite3/deltas/20230516154000_drop_reference_sha.go
@@ -18,6 +18,11 @@ import (
 	"context"
 	"database/sql"
 	"fmt"
+	"sort"
+
+	"github.com/lib/pq"
+	"github.com/matrix-org/dendrite/internal"
+	"golang.org/x/exp/slices"
 )
 
 func UpDropEventReferenceSHA(ctx context.Context, tx *sql.Tx) error {
@@ -52,8 +57,73 @@ func UpDropEventReferenceSHAPrevEvents(ctx context.Context, tx *sql.Tx) error {
 		return fmt.Errorf("tx.ExecContext: %w", err)
 	}
 
+	// figure out if there are duplicates
+	dupeRows, err := tx.QueryContext(ctx, `SELECT previous_event_id FROM _roomserver_previous_events GROUP BY previous_event_id HAVING count(previous_event_id) > 1`)
+	if err != nil {
+		return fmt.Errorf("failed to query duplicate event ids")
+	}
+	defer internal.CloseAndLogIfError(ctx, dupeRows, "failed to close rows")
+
+	var prevEvents []string
+	var prevEventID string
+	for dupeRows.Next() {
+		if err = dupeRows.Scan(&prevEventID); err != nil {
+			return err
+		}
+		prevEvents = append(prevEvents, prevEventID)
+	}
+	if dupeRows.Err() != nil {
+		return dupeRows.Err()
+	}
+
+	// if we found duplicates, check if we can combine them, e.g. they are in the same room
+	for _, dupeID := range prevEvents {
+		var dupeNIDsRows *sql.Rows
+		dupeNIDsRows, err = tx.QueryContext(ctx, `SELECT event_nids FROM _roomserver_previous_events WHERE previous_event_id = $1`, dupeID)
+		if err != nil {
+			return fmt.Errorf("failed to query duplicate event ids")
+		}
+		defer internal.CloseAndLogIfError(ctx, dupeNIDsRows, "failed to close rows")
+		var dupeNIDs []int64
+		for dupeNIDsRows.Next() {
+			var nids pq.Int64Array
+			if err = dupeNIDsRows.Scan(&nids); err != nil {
+				return err
+			}
+			dupeNIDs = append(dupeNIDs, nids...)
+		}
+
+		if dupeNIDsRows.Err() != nil {
+			return dupeNIDsRows.Err()
+		}
+		// dedupe NIDs
+		sort.Sort(nids(dupeNIDs))
+		dupeNIDs = slices.Compact(dupeNIDs)
+		// now that we have all NIDs, check which room they belong to
+		var roomCount int
+		err = tx.QueryRowContext(ctx, `SELECT count(distinct room_nid) FROM roomserver_events WHERE event_nid IN ($1)`, pq.Array(dupeNIDs)).Scan(&roomCount)
+		if err != nil {
+			return err
+		}
+		// if the events are from different rooms, that's bad and we can't continue
+		if roomCount > 1 {
+			return fmt.Errorf("detected events (%v) referenced for different rooms (%v)", dupeNIDs, roomCount)
+		}
+		// otherwise delete the dupes
+		_, err = tx.ExecContext(ctx, "DELETE FROM _roomserver_previous_events WHERE previous_event_id = $1", dupeID)
+		if err != nil {
+			return fmt.Errorf("unable to delete duplicates: %w", err)
+		}
+
+		// insert combined values
+		_, err = tx.ExecContext(ctx, "INSERT INTO _roomserver_previous_events (previous_event_id, event_nids) VALUES ($1, $2)", dupeID, pq.Array(dupeNIDs))
+		if err != nil {
+			return fmt.Errorf("unable to insert new event NIDs: %w", err)
+		}
+	}
+
 	// move data
-	if _, err := tx.ExecContext(ctx, `
+	if _, err = tx.ExecContext(ctx, `
 INSERT
     INTO roomserver_previous_events (
       previous_event_id, event_nids
@@ -64,9 +134,15 @@ INSERT
 		return fmt.Errorf("tx.ExecContext: %w", err)
 	}
 	// drop old table
-	_, err := tx.ExecContext(ctx, `DROP TABLE _roomserver_previous_events;`)
+	_, err = tx.ExecContext(ctx, `DROP TABLE _roomserver_previous_events;`)
 	if err != nil {
 		return fmt.Errorf("failed to execute upgrade: %w", err)
 	}
 	return nil
 }
+
+type nids []int64
+
+func (s nids) Len() int           { return len(s) }
+func (s nids) Less(i, j int) bool { return s[i] < s[j] }
+func (s nids) Swap(i, j int)      { s[i], s[j] = s[j], s[i] }
