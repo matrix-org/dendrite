11b284a639cb603dc6b702b335adc57c58137891
eyedeekay <hankhill19580@gmail.com>
2024-08-05 20:33:06 -0400

    Skip tests that require external services(Tor, I2P) when running in CI

--- contrib/dendrite-demo-i2p/main_test.go
+++ contrib/dendrite-demo-i2p/main_test.go
@@ -13,38 +13,42 @@ import (
 // Run the monolith: ./monolith.debug -test.coverprofile=/somewhere/to/dump/integrationcover.out DEVEL --config dendrite.yaml
 // Generate HTML with coverage: go tool cover -html=/somewhere/where/there/is/integrationcover.out -o cover.html
 // Source: https://dzone.com/articles/measuring-integration-test-coverage-rate-in-pouchc
-func TestMain(_ *testing.T) {
-	var (
-		args []string
-	)
+func TestMain(t *testing.T) {
+	if os.Getenv("CI") != "" {
+		t.Skip("skipping test, as no TOR/I2P client is available")
+	} else {
+		var (
+			args []string
+		)
 
-	for _, arg := range os.Args {
-		switch {
-		case strings.HasPrefix(arg, "DEVEL"):
-		case strings.HasPrefix(arg, "-test"):
-		default:
-			args = append(args, arg)
+		for _, arg := range os.Args {
+			switch {
+			case strings.HasPrefix(arg, "DEVEL"):
+			case strings.HasPrefix(arg, "-test"):
+			default:
+				args = append(args, arg)
+			}
 		}
-	}
-	// only run the tests if there are args to be passed
-	if len(args) <= 1 {
-		return
-	}
+		// only run the tests if there are args to be passed
+		if len(args) <= 1 {
+			return
+		}
 
-	waitCh := make(chan int, 1)
-	os.Args = args
-	go func() {
-		main()
-		close(waitCh)
-	}()
+		waitCh := make(chan int, 1)
+		os.Args = args
+		go func() {
+			main()
+			close(waitCh)
+		}()
 
-	signalCh := make(chan os.Signal, 1)
-	signal.Notify(signalCh, syscall.SIGINT, syscall.SIGQUIT, syscall.SIGTERM, syscall.SIGHUP)
+		signalCh := make(chan os.Signal, 1)
+		signal.Notify(signalCh, syscall.SIGINT, syscall.SIGQUIT, syscall.SIGTERM, syscall.SIGHUP)
 
-	select {
-	case <-signalCh:
-		return
-	case <-waitCh:
-		return
+		select {
+		case <-signalCh:
+			return
+		case <-waitCh:
+			return
+		}
 	}
 }
