8b769d2cc44025ceb9e5df7acd5a6a9c72f0b443
Quentin Gliech <quenting@element.io>
2024-10-17 17:21:25 +0200

    Fixup missed license headers

--- clientapi/auth/authtypes/flow.go
+++ clientapi/auth/authtypes/flow.go
@@ -1,16 +1,5 @@
-// Copyright Andrew Morgan <andrew@amorgan.xyz>
-//
-// Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at
-//
-//     http://www.apache.org/licenses/LICENSE-2.0
-//
-// Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and
 // Copyright 2024 New Vector Ltd.
+// Copyright 2017 Andrew Morgan <andrew@amorgan.xyz>
 //
 // SPDX-License-Identifier: AGPL-3.0-only
 // Please see LICENSE in the repository root for full details.
